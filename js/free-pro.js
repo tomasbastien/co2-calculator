4c55bddb18c1e7ef73167b761199230c2cfb7728
Bastien TOMAS <bastien.tomas@jaguar-network.com>
2023-08-22 17:46:29 +0200

    Adding Free Pro style

---
+++ js/free-pro.js
@@ -0,0 +1,22 @@
+L.Control.Watermark = L.Control.extend({
+		onAdd(map) {
+			const img = L.DomUtil.create('img');
+
+			img.src = './img/logo-fp-fondblanc.svg';
+			img.style.width = '50px';
+
+			return img;
+		},
+
+		onRemove(map) {
+			// Nothing to do here
+		}
+	});
+
+	L.control.watermark = function (opts) {
+		return new L.Control.Watermark(opts);
+	};
+	
+	L.control.watermark({position: 'bottomleft'}).addTo(map);
+
+	colors = ["#fffff","#cc0000"];
